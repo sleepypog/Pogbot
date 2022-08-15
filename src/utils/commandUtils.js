2ddf9d851fb4dc58945dc0693833b15dde599769
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-08-15 02:55:47 +0000

    invert dmPermission value and add check on requireAdmin

--- src/utils/commandUtils.js
+++ src/utils/commandUtils.js
@@ -5,11 +5,14 @@ import { PermissionsBitField } from 'discord.js';
  * @param {import("../commands").Command} command 
  */
 export function buildData(command) {
-	command.data.dmPermission = command.guildOnly ?? true;
+	command.data.dmPermission = !command.guildOnly ?? true;
 
 	const defaultPermissions = new PermissionsBitField('UseApplicationCommands');
 
 	if (command.requireAdmin) {
+		if (!command.guildOnly) {
+			throw new Error('Command cannot require admin if not limited to guilds!');
+		}
 		defaultPermissions.add('Administrator', 'ManageGuild');
 	} 
 
