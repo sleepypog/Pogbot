8d77e029e5799e9e6c6ca5e258f6ef60e977f13e
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-08-16 02:36:48 +0000

    command ready: dump

--- src/commands/dump.js
+++ src/commands/dump.js
@@ -1,16 +1,79 @@
+import { ApplicationCommandOptionType, codeBlock, inlineCode } from 'discord.js';
+import { fromArray } from '../utils/arrayUtils.js';
+import { buildList } from '../utils/stringUtils.js';
+
 /**
  * @type {import(".").Command}
  */
 export default {
 	guildOnly: true,
-	devOnly: true,
 	requireAdmin: true,
 	data: {
 		name: 'dump',
-		description: 'Dump information about this guild.',
-		dmPermission: false
+		description: 'Dump information for debugging.',
+		options: [
+			{
+				name: 'guild',
+				description: 'Multiple guild data.',
+				type: ApplicationCommandOptionType.SubcommandGroup,
+				options: [
+					{
+						name: 'full',
+						description: 'Dump all guild data as JSON.',
+						type: ApplicationCommandOptionType.Subcommand
+					},
+					{
+						name: 'triggers',
+						description: 'Dump all triggers as an list.',
+						type: ApplicationCommandOptionType.Subcommand
+					},
+					{
+						name: 'channels',
+						description: 'Dump all channels as an list.',
+						type: ApplicationCommandOptionType.Subcommand
+					},
+					{
+						name: 'master',
+						description: 'Dump the ID for the current master.',
+						type: ApplicationCommandOptionType.Subcommand
+					}
+				]
+			},
+		]
 	},
-	execute: async (interaction) => {
-		interaction.reply('Hey!');
+	execute: async (client, interaction) => {
+		if (interaction.options.getSubcommandGroup(true) === 'guild') {
+			const guild = await client.database.getGuild(interaction.guildId);
+			switch (interaction.options.getSubcommand(true)) {
+			case 'full': {
+				await interaction.reply({ 
+					content: codeBlock('json', JSON.stringify(guild.toJSON(), null, '\t')), 
+					ephemeral: true
+				});
+				break;
+			}
+			case 'triggers': {
+				await interaction.reply({ 
+					content: codeBlock(buildList(fromArray(guild.getDataValue('triggers')))), 
+					ephemeral: true
+				});
+				break;
+			}
+			case 'channels': {
+				await interaction.reply({ 
+					content: codeBlock(buildList(fromArray(guild.getDataValue('channels')))), 
+					ephemeral: true
+				});
+				break;
+			}
+			case 'master': {
+				await interaction.reply({ 
+					content: inlineCode(guild.getDataValue('master')), 
+					ephemeral: true
+				});
+				break;
+			}
+			}
+		}
 	}
 };
