a369de4bba5d74eb960f97d5f69a6781c12c13f5
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-08-16 02:36:57 +0000

    command ready: score

---
+++ src/commands/score.js
@@ -0,0 +1,28 @@
+import { ApplicationCommandOptionType } from 'discord.js';
+
+/**
+ * @type {import(".").Command}
+ */
+export default {
+	guildOnly: true,
+	requireAdmin: true,
+	data: {
+		name: 'score',
+		description: 'Get the amount of points you (or someone else has).',
+		options: [
+			{
+				name: 'user',
+				description: 'The user to fetch score for. Defaults to you.',
+				type: ApplicationCommandOptionType.User,
+			},
+		]
+	},
+	execute: async (client, interaction) => {
+		const who = interaction.options.getUser('user') ?? interaction.user;
+		const score = (await client.database.getMember(interaction.guildId, who.id)).getDataValue('score');
+		await interaction.reply({
+			content: (who.id === interaction.user.id ? 'You have ' : '<@' + who.id + '> has ') + score + ' points in this guild.',
+			ephemeral: true
+		});
+	}
+};
