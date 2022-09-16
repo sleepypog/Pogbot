a183c8643e90331745acfeb43212e0698616c2b4
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-09-16 01:16:21 +0000

    use member id instead of db id

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from 'discord.js';
+import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
 
 /**
  * @type {import('.').Command}
@@ -8,10 +8,38 @@ export default {
 	requireAdmin: false,
 	data: {
 		name: 'leaderboard',
-		description: 'See top scores for this guild.'
+		description: 'See top scores for this guild.',
+		options: [
+			{
+				name: 'amount',
+				description: 'How many places to fetch.',
+				type: ApplicationCommandOptionType.Integer,
+				minValue: 3,
+				maxValue: 10
+			}
+		]
 	},
 	execute: async (client, interaction) => {
+		const members = await client.database.getTopMembers(interaction.guildId);
+
+		if (members.length === 0) {
+			return await interaction.reply('😮 This is awkward, ' + interaction.guild.name + ' does not have any saved scores at all! Try using `/score` first.');
+		}
+
 		const embed = new EmbedBuilder();
-		const guild = await client.database.getTopMembers(interaction.guildId);
+
+		embed.setTitle('Leaderboard for ' + interaction.guild.name);
+		embed.setColor('Greyple');
+
+		const scores = [];
+		members.forEach((member) => {
+			scores.push('<@' + member.get('member_id') + '> ' + member.get('score') + ' points');
+		});
+
+		embed.setDescription(scores.join('\n'));
+
+		await interaction.reply({
+			embeds: [ embed ]
+		});
 	}
 };
