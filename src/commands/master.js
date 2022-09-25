79a868401a12e097e57089d3a7c812adeb2e013d
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-09-25 02:10:53 +0000

    fix master functionality and listeners

--- src/commands/master.js
+++ src/commands/master.js
@@ -9,17 +9,16 @@ export default {
 		description: 'See the current Pog master for the guild.',
 	},
 	execute: async (client, interaction) => {
-		const guild = await client.database.getGuild(interaction.guildId);
-		const master = guild.get('master');
+		const master = await (await client.database.getGuild(interaction.guildId)).get('master');
 
 		if (master === '') {
 			await interaction.reply({
 				content: 'This guild has not crowned their Pog Master yet.',
 			});
 		} else {
-			const { nickname } = await interaction.guild.members.fetch(master);
+			const { displayName } = await interaction.guild.members.fetch(master);
 			await interaction.reply({
-				content: '👑 The Pog Master of ' + interaction.guild.name + ' is ' + nickname
+				content: '👑 The Pog Master of ' + interaction.guild.name + ' is ' + displayName
 			});
 		}
 	}
